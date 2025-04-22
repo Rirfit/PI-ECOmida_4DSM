de9d9b62b38ac23ed4cf1f11985a1eaf528eab77
Kauan <pireskauan80@gmail.com>
2025-04-22 08:26:40 -0300

    mudanças pequenas

--- PI/src/pages/sobre/Sobre.jsx
+++ PI/src/pages/sobre/Sobre.jsx
@@ -8,7 +8,7 @@ export const Sobre = () => {
     <>
       <Header />
       <h4 className="texto">
-        Somos uma ONG comprometida em conscientizar a sociedade sobre o desperdício de alimentos e promover práticas mais sustentáveis no nosso dia a dia. Acreditamos que pequenas mudanças podem fazer uma grande diferença no nosso impacto no meio ambiente e na vida das pessoas. Nosso objetivo é criar uma rede de apoio e educação, fornecendo informações valiosas sobre como reduzir o desperdício, reaproveitar alimentos e ajudar aqueles que mais precisam.
+        Somos um projeto comprometida em conscientizar a sociedade sobre o desperdício de alimentos e promover práticas mais sustentáveis no nosso dia a dia. Acreditamos que pequenas mudanças podem fazer uma grande diferença no nosso impacto no meio ambiente e na vida das pessoas. Nosso objetivo é criar uma rede de apoio e educação, fornecendo informações valiosas sobre como reduzir o desperdício, reaproveitar alimentos e ajudar aqueles que mais precisam.
       </h4>
       <div className="imagemS"></div>
       <Footer/>
