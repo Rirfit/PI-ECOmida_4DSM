64e6de7d7b865951de35991d665436d2914b7a8c
Rirfit <lilrirfit@gmail.com>
2025-06-09 23:11:25 -0300

    Listagem das receitas

--- PI/src/pages/carnes/Carnes.jsx
+++ PI/src/pages/carnes/Carnes.jsx
@@ -1,48 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import "../receitas/Receitas.css"
 import { Link } from 'react-router-dom';
-import legumes from '../../assets/legumes-carne.jpg'
-import lagarto from '../../assets/lagarto-molho-madeira.jpg'
-import panqueca from '../../assets/panqueca-de-carne.jpg'
-import picanha from '../../assets/picanha-na-manteiga.jpeg'
+import legumes from '../../assets/legumes-carne.jpg';
+import lagarto from '../../assets/lagarto-molho-madeira.jpg';
+import panqueca from '../../assets/panqueca-de-carne.jpg';
+import picanha from '../../assets/picanha-na-manteiga.jpeg';
 
 function Carnes() {
-    return (
-        <>
-            <Header />
-            <div className="pagina">
+  const [receitasDinamicas, setReceitasDinamicas] = useState([]);
+  const [erro, setErro] = useState(null);
 
-                <div className="receitas-container">
+  useEffect(() => {
+    fetch('http://localhost:5000/receitas?categoria=carnes')
+      .then((res) => {
+        if (!res.ok) throw new Error('Erro ao buscar receitas');
+        return res.json();
+      })
+      .then((data) => {
+        setReceitasDinamicas(data.receitas);
+      })
+      .catch((err) => {
+        setErro(err.message);
+      });
+  }, []);
 
-                    <Link to="/Panqueca" className="categoria">
-                        <img src={panqueca} alt="panqueca" />
-                        <h4>Panqueca de carne</h4>
-                    </Link>
+  const baseUrlImagem = 'http://localhost:5000/static/';
 
-                    <Link to="/Lagarto" className="categoria">
-                        <img src={lagarto} alt="lagarto" />
-                        <h4>Lagarto ao molho madeira</h4>
-                    </Link>
+  return (
+    <>
+      <Header />
+      <div className="pagina">
+        <h2>Nossas receitas</h2>
+        <div className="receitas-container">
+          <Link to="/Panqueca" className="categoria">
+            <img src={panqueca} alt="panqueca" />
+            <h4>Panqueca de carne</h4>
+          </Link>
 
-                    <Link to="/LegumesCarne" className="categoria">
-                        <img src={legumes} alt="legumes" />
-                        <h4>Legumes com carne</h4>
-                    </Link>
+          <Link to="/Lagarto" className="categoria">
+            <img src={lagarto} alt="lagarto" />
+            <h4>Lagarto ao molho madeira</h4>
+          </Link>
 
-                    <Link to="/PicanhaManteiga" className="categoria">
-                        <img src={picanha} alt="picanha" />
-                        <h4>Picanha na manteiga</h4>
-                    </Link>
-                </div>
-            </div>
-            <Footer />
-        </>
+          <Link to="/LegumesCarne" className="categoria">
+            <img src={legumes} alt="legumes" />
+            <h4>Legumes com carne</h4>
+          </Link>
 
+          <Link to="/PicanhaManteiga" className="categoria">
+            <img src={picanha} alt="picanha" />
+            <h4>Picanha na manteiga</h4>
+          </Link>
+        </div>
 
+        <h2>Receitas de vocês</h2>
+        {erro && <p style={{ color: 'red' }}>{erro}</p>}
+        <div className="receitas-container">
+          {receitasDinamicas.length === 0 && !erro && <p>Carregando receitas...</p>}
 
-    )
-};
+          {receitasDinamicas.map((receita) => (
+            <Link key={receita._id} to={`/receita/${receita._id}`} className="categoria">
+              <img
+                src={receita.imagem ? baseUrlImagem + receita.imagem : 'https://via.placeholder.com/150'}
+                alt={receita.titulo}
+              />
+              <h4>{receita.titulo}</h4>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
 
 export default Carnes;
--- PI/src/pages/peixes/Peixes.jsx
+++ PI/src/pages/peixes/Peixes.jsx
@@ -1,48 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
-import "../receitas/Receitas.css"
+import "../receitas/Receitas.css";
 import { Link } from 'react-router-dom';
-import bolinhoPeixe from '../../assets/bolinhoPeixe.jpg'
-import bolinhoBacalhau from '../../assets/bolinhoBacalhau.jpg'
-import arrozPeixe from '../../assets/arrozPeixe.jpg'
-import gratinadoPeixe from '../../assets/gratinadoPeixe.jpg'
+import bolinhoPeixe from '../../assets/bolinhoPeixe.jpg';
+import bolinhoBacalhau from '../../assets/bolinhoBacalhau.jpg';
+import arrozPeixe from '../../assets/arrozPeixe.jpg';
+import gratinadoPeixe from '../../assets/gratinadoPeixe.jpg';
 
 function Peixes() {
-    return (
-        <>
-            <Header />
-            <div className="pagina">
+  const [receitasDinamicas, setReceitasDinamicas] = useState([]);
+  const [erro, setErro] = useState(null);
 
-                <div className="receitas-container">
+  useEffect(() => {
+    fetch('http://localhost:5000/receitas?categoria=peixes')
+      .then((res) => {
+        if (!res.ok) throw new Error('Erro ao buscar receitas');
+        return res.json();
+      })
+      .then((data) => {
+        setReceitasDinamicas(data.receitas);
+      })
+      .catch((err) => {
+        setErro(err.message);
+      });
+  }, []);
 
-                    <Link to="/BolinhoPeixe" className="categoria">
-                        <img src={bolinhoPeixe} alt="Bolinho de peixe com batata" />
-                        <h4>Bolinho de peixe com batata</h4>
-                    </Link>
+  const baseUrlImagem = 'http://localhost:5000/static/';
 
-                    <Link to="/BolinhoBacalhau" className="categoria">
-                        <img src={bolinhoBacalhau} alt="Bolinho de Bacalhau" />
-                        <h4>Bolinho de bacalhau</h4>
-                    </Link>
+  return (
+    <>
+      <Header />
+      <div className="pagina">
 
-                    <Link to="/ArrozPeixe" className="categoria">
-                        <img src={arrozPeixe} alt="Arroz cremoso de peixe" />
-                        <h4>Arroz cremoso de peixe</h4>
-                    </Link>
+        <h2>Nossas receitas</h2>
+        <div className="receitas-container">
 
-                    <Link to="/GratinadoPeixe" className="categoria">
-                        <img src={gratinadoPeixe} alt="gratinado de peixe" />
-                        <h4>Gratinado de peixe</h4>
-                    </Link>
-                </div>
-            </div>
-            <Footer />
-        </>
+          <Link to="/BolinhoPeixe" className="categoria">
+            <img src={bolinhoPeixe} alt="Bolinho de peixe com batata" />
+            <h4>Bolinho de peixe com batata</h4>
+          </Link>
 
+          <Link to="/BolinhoBacalhau" className="categoria">
+            <img src={bolinhoBacalhau} alt="Bolinho de Bacalhau" />
+            <h4>Bolinho de bacalhau</h4>
+          </Link>
 
+          <Link to="/ArrozPeixe" className="categoria">
+            <img src={arrozPeixe} alt="Arroz cremoso de peixe" />
+            <h4>Arroz cremoso de peixe</h4>
+          </Link>
 
-    )
-};
+          <Link to="/GratinadoPeixe" className="categoria">
+            <img src={gratinadoPeixe} alt="gratinado de peixe" />
+            <h4>Gratinado de peixe</h4>
+          </Link>
+        </div>
+
+        <h2>Receitas de vocês</h2>
+        {erro && <p style={{ color: 'red' }}>{erro}</p>}
+        <div className="receitas-container">
+          {receitasDinamicas.length === 0 && !erro && <p>Carregando receitas...</p>}
+
+          {receitasDinamicas.map((receita) => (
+            <Link key={receita._id} to={`/receita/${receita._id}`} className="categoria">
+              <img
+                src={receita.imagem ? baseUrlImagem + receita.imagem : 'https://via.placeholder.com/150'}
+                alt={receita.titulo}
+              />
+              <h4>{receita.titulo}</h4>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
 
 export default Peixes;
